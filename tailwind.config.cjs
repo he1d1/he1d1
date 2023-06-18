a50a21a0b0cf018565c67dfa46234ec9cde51004
he1d1 <hey@heidi.codes>
2023-06-18 13:11:06 +0100

    Created basic blog with Astro

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      background: "#161616",
+      brand: "#d4bbff",
+      primary: "#f4f4f4",
+      secondary: "#c6c6c6",
+      subtle: "#393939",
+      layer: "#262626",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
