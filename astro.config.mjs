6b9532c77c1ebb9a4afa3b6d29bb35b231518c50
Heidi <hey@heidi.codes>
2025-11-02 01:20:07 +0000

    Add UnoCSS and add basic content

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import UnoCSS from 'unocss/astro'
+import presetMini from '@unocss/preset-mini'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+      presets: [ presetMini({ dark: 'media' }) ],
+      transformers: [
+        transformerVariantGroup(),
+      ],
+    }),
+  ],
 });
